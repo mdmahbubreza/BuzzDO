48fcff2ace4c3e4d9decb24962000c9d576b9258
Mohd Faiz Reza <mohdfaiz694reza@gmail.com>
2024-03-31 13:39:48 +0530

    add some code in tasks.html, tasks-styles.css, and tasks-scripts.js

--- tasks-scripts.js
+++ tasks-scripts.js
@@ -1,10 +1,9 @@
 const inputBox = document.getElementById("input-task");
 const listContainer = document.getElementById("list-container");
-function addTask(){
-  if(inputBox.value == ''){
+function addTask() {
+  if (inputBox.value == "") {
     alert("You must write something!");
-  }
-  else{
+  } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
@@ -12,25 +11,43 @@ function addTask(){
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
-  inputBox.value = '';
+  inputBox.value = "";
   saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-  if(e.target.tagName === "LI"){
-    e.target.classList.toggle("checked");
-    saveData();
-  }
-  else if(e.target.tagName === "SPAN"){
-    e.target.parentElement.remove();
-    saveData();
-  }
-}, false);
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
 
-function saveData(){
+function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
-function showTask(){
+function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
+
+// yaha menubtn ka code ha
+let menucontent = document.getElementById("menucontent");
+
+let btn = document.getElementById("menubtn");
+let butn = false;
+btn.addEventListener("click", () => {
+  if (!butn) {
+    menucontent.style.left = "0";
+    butn = true;
+  } else {
+    menucontent.style.left = "-340px"
+    butn = false;
+  }
+});
